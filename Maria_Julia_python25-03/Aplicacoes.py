3b0dee939025527762bd1c442e68fb1c5906e59f
mariajuliacintra <mariajuliacintra31@gmail.com>
2024-06-03 11:35:50 -0300

    03-06

--- Maria_Julia_python25-03/Aplicacoes.py
+++ Maria_Julia_python25-03/Aplicacoes.py
@@ -7,4 +7,35 @@ for i in range(1, len(lista)):
         maiorNumero = lista[i]
 print("Numero Maior da Lista:", maiorNumero)
 
+#Exemplo 2
+my_list = [17, 3, 11, 5, 1, 9, 7, 15, 18]
+maior = my_list[0]
+for i in my_list:
+    if i > maior:
+        maior = i
+print("Valor Maior2:", maior)
+
+# exemplo 3
+ultimaLista = my_list[:]
+mel = ultimaLista[0]
+for i in ultimaLista[1:]:
+    if i > mel:
+      mel = i
+print(mel)
+
+#Encontrar a localização de um determinado elemento dentro de uma lista
+frutas = ["abacaxi", "maçã", "pêra", "mamão", "uva", "melancia"]
+elemento = "melancia"
+achado = False
+
+for i in range(len(frutas)):
+    achado = frutas[i] == elemento
+    if achado:
+        break
+
+if achado:
+    print("Elemento encontrado no indice:", i)
+else:
+    print("Elemento não encontrado!!!")
+
 time.sleep(3)
